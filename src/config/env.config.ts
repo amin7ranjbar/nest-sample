1e9a60ea29fc8243e9122a63b014bcafe9333b7f
amin7ranjbar <amin7ranjbar@gmail.com>
2022-12-08 21:49:43 +0330

    fix: add new env to joi check

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -9,7 +9,12 @@ export const envValidationSchema = Joi.object({
   PGADMIN_DEFAULT_EMAIL: Joi.string().required(),
   PGADMIN_DEFAULT_PASSWORD: Joi.string().required(),
   JWT_SECRET_KEY: Joi.string().required(),
-  MAX_FILE_SIZE: Joi.string().required(),
+  MAX_FILE_SIZE: Joi.number().required(),
   UPLOAD_LOCATION: Joi.string().required(),
   UPLOAD_URL: Joi.string().required(),
+  REDIS_HOST: Joi.string().required(),
+  REDIS_PORT: Joi.number().required(),
+  REDIS_PASSWORD: Joi.string().required(),
+  COMMANDER_USERNAME: Joi.string().required(),
+  COMMANDER_PASSWORD: Joi.string().required(),
 });
